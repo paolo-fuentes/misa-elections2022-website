14b00845ab7d51d759cd3d1a0d47e6982fec8ce4
Arvin Enriquez <arvin.enriquez528@gmail.com>
2021-04-25 18:57:53 +0800

    Initial commit

---
+++ src/layout/MainNavbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <navbar
+    position="fixed"
+    type="primary"
+    :transparent="transparent"
+    :color-on-scroll="colorOnScroll"
+    menu-classes="ml-auto"
+  >
+    <template>
+      <router-link v-popover:popover1 class="navbar-brand" to="/">
+        <img class="" src="img/now-logo.png" alt="" style="height: 42px; width: 42px;"/>
+      </router-link>
+      
+    </template>
+    <template slot="navbar-menu">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="#/"
+        >
+          <p>Home</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="#/about"
+        >
+          <p>About</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="#/"
+        >
+          <p>Services</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="#/"
+        >
+          <p>Contact Us</p>
+        </a>
+      </li>
+    </template>
+  </navbar>
+</template>
+
+<script>
+import { DropDown, Navbar, NavLink } from '@/components';
+import { Popover } from 'element-ui';
+export default {
+  name: 'main-navbar',
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number
+  },
+  components: {
+    Navbar,
+    [Popover.name]: Popover
+  }
+};
+</script>
+
+<style scoped></style>
