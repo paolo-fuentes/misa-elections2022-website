7b3d9951a844aef361e6bea9261b0eea69991bd8
Lorenzo Miguel Franco <lorenzomiguelfranco@gmail.com>
2022-03-07 17:23:36 +0800

    made login arrows invisible

--- src/pages/ElectionsLogin.vue
+++ src/pages/ElectionsLogin.vue
@@ -77,6 +77,18 @@ export default {
 </script>
 <style>
 
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+
 .custom-field {
   position: relative;
   font-size: 14px;
